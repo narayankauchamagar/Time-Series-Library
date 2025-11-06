7c840e6483d807d52727d7e89ee614b5ffdc2352
Narayan Thapa <narayankauchamagar@gmail.com>
2025-11-06 21:40:38 +0545

    Run Experiment

--- nepse_run.py
+++ nepse_run.py
@@ -221,42 +221,40 @@ def run_experiment(**arg_overrides):
 
     Exp = Exp_Long_Term_Forecast
 
-    # Run experiments
-    for ii in range(args.itr):
-        # Initialize experiment
-        exp = Exp(args)
-        setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_expand{}_dc{}_fc{}_eb{}_dt{}_{}_{}'.format(
-            args.task_name,
-            args.model_id,
-            args.model,
-            args.data,
-            args.features,
-            args.seq_len,
-            args.label_len,
-            args.pred_len,
-            args.d_model,
-            args.n_heads,
-            args.e_layers,
-            args.d_layers,
-            args.d_ff,
-            args.expand,
-            args.d_conv,
-            args.factor,
-            args.embed,
-            args.distil,
-            args.des, ii)
+    # Initialize experiment
+    exp = Exp(args)
+    setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_expand{}_dc{}_fc{}_eb{}_dt{}_{}_{}'.format(
+        args.task_name,
+        args.model_id,
+        args.model,
+        args.data,
+        args.features,
+        args.seq_len,
+        args.label_len,
+        args.pred_len,
+        args.d_model,
+        args.n_heads,
+        args.e_layers,
+        args.d_layers,
+        args.d_ff,
+        args.expand,
+        args.d_conv,
+        args.factor,
+        args.embed,
+        args.distil,
+        args.des, ii)
 
-        # Training
-        print(f'>>>>>>>start training : {setting}>>>>>>>>>>>>>>>>>>>>>>>>>>')
-        exp.train(setting)
-        # Testing
-        print(f'>>>>>>>testing : {setting}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-        metrics, gt, preds = exp.test(setting)
-        # Clear cache
-        if args.gpu_type == 'mps':
-            torch.mps.empty_cache()
-        elif args.gpu_type == 'cuda':
-            torch.cuda.empty_cache()
+    # Training
+    print(f'>>>>>>>start training : {setting}>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    exp.train(setting)
+    # Testing
+    print(f'>>>>>>>testing : {setting}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+    metrics, gt, preds = exp.test(setting)
+    # Clear cache
+    if args.gpu_type == 'mps':
+        torch.mps.empty_cache()
+    elif args.gpu_type == 'cuda':
+        torch.cuda.empty_cache()
     print("\nExperiment completed successfully!")
     return metrics, gt, preds
 
